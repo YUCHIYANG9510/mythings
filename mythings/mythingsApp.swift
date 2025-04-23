a0a5561bea0a36a3b3ebf424e0983dc699c74b33
daisy9510 <daisy.yc.yang@gmail.com>
2025-04-23 22:42:36 +0800

    Initial Commit

---
+++ mythings/mythingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mythingsApp.swift
+//  mythings
+//
+//  Created by Designer on 2025/4/23.
+//
+
+import SwiftUI
+
+@main
+struct mythingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mythingsTests/mythingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  mythingsTests.swift
+//  mythingsTests
+//
+//  Created by Designer on 2025/4/23.
+//
+
+import Testing
+@testable import mythings
+
+struct mythingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
