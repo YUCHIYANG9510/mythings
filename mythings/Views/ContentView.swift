bdf82320e5d9de6ee3cd244a0988eb5605a00680
daisy9510 <daisy.yc.yang@gmail.com>
2025-04-29 16:06:20 +0800

    Manage swift files.

--- mythings/Views/ContentView.swift
+++ mythings/Views/ContentView.swift
@@ -10,35 +10,8 @@ import PhotosUI
 import UIKit
 import Foundation
 
-struct Item: Identifiable, Codable {
-    let id: UUID
-    let imageName: String
-    let brand: String
-    let category: String
-    let name: String
-    let price: String
 
-    init(id: UUID = UUID(), imageName: String, brand: String, category: String, name: String, price: String) {
-        self.id = id
-        self.imageName = imageName
-        self.brand = brand
-        self.category = category
-        self.name = name
-        self.price = price
-    }
-}
 
-struct Category: Identifiable, Codable, Hashable {
-    let id: UUID
-    var name: String
-    var color: String
-    
-    init(id: UUID = UUID(), name: String, color: String = "blue") {
-        self.id = id
-        self.name = name
-        self.color = color
-    }
-}
 
 extension FileManager {
     static var documentsDirectory: URL {
@@ -46,65 +19,7 @@ extension FileManager {
     }
 }
 
-class CategoryStore: ObservableObject {
-    @Published var categories: [Category] = []
-    
-    private var savePath: URL {
-        FileManager.documentsDirectory.appendingPathComponent("categories.json")
-    }
-    init() {
-            loadCategories()
-            
-            // Add default categories if none exist
-            if categories.isEmpty {
-                categories = [
-                    Category(name: "3C Device", color: "blue"),
-                    Category(name: "Furniture", color: "green"),
-                    Category(name: "Kitchen", color: "orange"),
-                    Category(name: "Clothes", color: "purple"),
-                    Category(name: "Shoes", color: "red"),
-                    Category(name: "Bags", color: "indigo")
-                ]
-                saveCategories()
-            }
-        }
-        
-        func addCategory(name: String, color: String = "blue") {
-            let newCategory = Category(name: name, color: color)
-            categories.append(newCategory)
-            saveCategories()
-        }
-        
-        func deleteCategory(at indexSet: IndexSet) {
-            categories.remove(atOffsets: indexSet)
-            saveCategories()
-        }
-        
-        func updateCategory(category: Category) {
-            if let index = categories.firstIndex(where: { $0.id == category.id }) {
-                categories[index] = category
-                saveCategories()
-            }
-        }
-        
-        private func saveCategories() {
-            do {
-                let data = try JSONEncoder().encode(categories)
-                try data.write(to: savePath)
-            } catch {
-                print("Failed to save categories: \(error)")
-            }
-        }
-        
-        private func loadCategories() {
-            do {
-                let data = try Data(contentsOf: savePath)
-                categories = try JSONDecoder().decode([Category].self, from: data)
-            } catch {
-                print("Failed to load categories or no data yet: \(error)")
-            }
-        }
-    }
+
 
 struct ContentView: View {
     @State private var selectedCategory = "All"
@@ -498,20 +413,7 @@ struct ContentView: View {
             }
         }
         
-        func colorForName(_ name: String) -> Color {
-            switch name {
-            case "blue": return .blue
-            case "green": return .green
-            case "red": return .red
-            case "purple": return .purple
-            case "indigo": return .indigo
-            case "orange": return .orange
-            case "pink": return .pink
-            case "yellow": return .yellow
-            case "teal": return .teal
-            default: return .blue
-            }
-        }
+        
     }
 
     struct AddCategoryView: View {
